5baaef037b29aeaaae916683d3e10834471aa3e4
Jarrett Jennings <111404364+codingjarrett@users.noreply.github.com>
2023-03-15 13:06:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,66 +1,44 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const handlebars = require('express-handlebars');
-const path = require('path');
-const dotenv = require('dotenv');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// Import controllers
-const authController = require('./controllers/auth');
-const postController = require('./controllers/posts');
-
-// Load environment variables
-dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-// Set up session store
-const sessionStore = new SequelizeStore({
-  db: sequelize,
-});
+const hbs = exphbs.create({ helpers });
 
-// Set up session middleware
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 86400000 },
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
   })
-);
+};
 
-// Set up Handlebars as template engine
-app.engine(
-  'handlebars',
-  handlebars({
-    layoutsDir: path.join(__dirname, 'views', 'layouts'),
-    partialsDir: path.join(__dirname, 'views', 'partials'),
-    defaultLayout: 'main',
-  })
-);
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// Set up static assets
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set up request body parser
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(routes);
 
-// Set up routes
-app.use('/auth', authController);
-app.use('/posts', postController);
-
-// Set up error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).render('error', { error: err });
-});
-
-// Sync database and start server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
